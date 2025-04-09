4b0cb835302718e8cf0a6efa6fb9b2eef8679e13
RajCoder9931 <tiwariraviraj50@gmail.com>
2025-04-09 21:52:55 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobboard-jfgj.onrender.com',
     credentials:true
 }
 const _dirname = path.resolve(); // add the path intilization
